97714d5cd1960b8fddd16ccbfed3a6bb89973c1f
hank <hank8451@gmail.com>
2021-01-03 11:58:43 +0800

    Product CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
-  before_action :find_product
+  before_action :find_product, only:[:edit, :destroy, :update]
 
   def index
-    @products = Product.all
+    @products = Product.all.order(:id)
   end
 
   def new
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to root_path, notice: "創立成功"
     else
       render :new
     end
@@ -22,16 +22,24 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    if @product.update(product_params)
+        redirect_to root_path, notice: "更新成功"
+    else
+        render :edit
+    end
+  end
+
   def destroy
     @product = Product.find(params[:id])
     if @product.destroy
-      redirect_to root_path
+      redirect_to root_path, notice: "刪除成功"
     end
   end
 
   private
   def find_product
-
+    @product = Product.find(params[:id])
   end
 
   def product_params
